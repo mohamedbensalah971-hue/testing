ddfa2689c6029d8cd5bc48901b92a8b641f1df14
Mohamed <mohamedbensalah971@gmail.com>
2026-05-08 02:41:10 +0100

    edition 164

--- app/src/main/java/com/quickchat/app/util/ConversationAnalyzer.kt
+++ app/src/main/java/com/quickchat/app/util/ConversationAnalyzer.kt
@@ -205,4 +205,4 @@ object ConversationAnalyzer {
         return lastMessageTime + averageInterval
     }
 }
-//testfinalfaresaloalofinalfinalfinal yassou3.3
+//testfinalfaresaloalofinalfinalfinal yassou3.4
